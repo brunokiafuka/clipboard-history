f35490f2b93c5ee24bc9c0e83305cb70210efa19
Faustino Kialungila <faustino.kialungila@gmail.com>
2019-10-05 07:50:31 +0200

    add icon and dock-icon

--- utils/index.js
+++ utils/index.js
@@ -1,26 +1,26 @@
-const APP_WIDTH = 300
-const LINE_HEIGHT = 30
-const PREFERENCES_HEIGHT = 110
-const DEFAULT_HEIGHT = LINE_HEIGHT + (PREFERENCES_HEIGHT / 2)
-
+const APP_WIDTH = 300;
+const LINE_HEIGHT = 30;
+const PREFERENCES_HEIGHT = 110;
+const DEFAULT_HEIGHT = LINE_HEIGHT + PREFERENCES_HEIGHT / 2;
 
 const setWindowSize = (
   windowInstance,
   width = APP_WIDTH,
   height = DEFAULT_HEIGHT
-) => windowInstance.setSize(width, height)
-
+) => windowInstance.setSize(width, height);
 
 const setWindowSizeAndVibrancy = (windowInstance, hasData, dataLength) => {
-    if (!hasData) {
-      windowInstance.setVibrancy('ultra-dark')
-      return setWindowSize(windowInstance, APP_WIDTH, DEFAULT_HEIGHT)
-    }
-    windowInstance.setVibrancy('dark')
-    const appHeight = dataLength > 15 ? LINE_HEIGHT * 15 : (LINE_HEIGHT * dataLength) + PREFERENCES_HEIGHT
-    return setWindowSize(windowInstance, APP_WIDTH, appHeight)
-}
-
+  if (!hasData) {
+    windowInstance.setVibrancy("ultra-dark");
+    return setWindowSize(windowInstance, APP_WIDTH, DEFAULT_HEIGHT);
+  }
+  windowInstance.setVibrancy("dark");
+  const appHeight =
+    dataLength > 15
+      ? LINE_HEIGHT * 15
+      : LINE_HEIGHT * dataLength + PREFERENCES_HEIGHT;
+  return setWindowSize(windowInstance, APP_WIDTH, appHeight);
+};
 
 module.exports = {
   APP_WIDTH,
@@ -28,5 +28,4 @@ module.exports = {
   PREFERENCES_HEIGHT,
   setWindowSizeAndVibrancy,
   setWindowSize
-
-}
+};
